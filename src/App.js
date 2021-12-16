b3f4bebed20dac7aea6e512db8a0ae2774617487
drealjstar <drealjstar@yahoo.com>
2021-12-16 17:53:09 +0100

    modify code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Note from "./Component/note";
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./note.css";
+
+
+
+function Note(){
+    return(
+        <div className="textBody">
+        <div className="sideBar"></div>
+        <div className="mainPage"></div>
+        
+        </div>
+    )
+}
+
+
+export default Note;
